0814086ecd55bc3d2201b6d9c2d4e500d3cb011b
Brullrr <rbrull@yahoo.com>
2020-11-15 18:26:22 +0900

    changed brower route to index

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
 import Restaurant from './Restaurant/Restaurant';
-import { BrowserRouter } from 'react-router-dom';
+
 
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    
       <Restaurant />
-    </BrowserRouter>
     
   );
 }
