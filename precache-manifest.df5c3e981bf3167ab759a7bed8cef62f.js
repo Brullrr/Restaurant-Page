7ea0321e58f25c964dc219946b616f7ba093e107
Brullrr <rbrull@yahoo.com>
2020-11-15 17:19:16 +0900

    Updates

---
+++ precache-manifest.df5c3e981bf3167ab759a7bed8cef62f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73a95b2b7ef827b15525c315e4b6ac9e",
+    "url": "/Restaurant-Page/index.html"
+  },
+  {
+    "revision": "add13ed97af789c522bf",
+    "url": "/Restaurant-Page/static/css/main.3ec97352.chunk.css"
+  },
+  {
+    "revision": "c8464e9f996a91a7fc0e",
+    "url": "/Restaurant-Page/static/js/2.36215a14.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Restaurant-Page/static/js/2.36215a14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "add13ed97af789c522bf",
+    "url": "/Restaurant-Page/static/js/main.dd1f465d.chunk.js"
+  },
+  {
+    "revision": "2abfc07a5a5b25600f7f",
+    "url": "/Restaurant-Page/static/js/runtime-main.32c6882b.js"
+  }
+]);
