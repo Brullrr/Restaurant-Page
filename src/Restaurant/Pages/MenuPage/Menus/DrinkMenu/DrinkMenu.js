c0102defa24f3bb03806c924cb35718404f1d4b3
Brullrr <rbrull@yahoo.com>
2020-11-15 18:49:38 +0900

    adding pictures

--- src/Restaurant/Pages/MenuPage/Menus/DrinkMenu/DrinkMenu.js
+++ src/Restaurant/Pages/MenuPage/Menus/DrinkMenu/DrinkMenu.js
@@ -9,7 +9,7 @@ const drinkMenu = () => {
         <div className={classes.DrinkPicture}>
             <div className={classes.Overlay}></div>
             <Link to='/menu/drinks'><button className={classes.InnerButton}> {'>'} </button></Link>
-            <img className={classes.DrinkImage} src='/Images/Juice.jpg' alt="drink"></img>
+            <img className={classes.DrinkImage} src='./Images/Juice.jpg' alt="drink"></img>
             
         </div>
         <p className={classes.Description}>Sporting a variety of flavors our beverages are sure to quench any thirst level. Both alcoholic and non-alcoholic are available.</p>
